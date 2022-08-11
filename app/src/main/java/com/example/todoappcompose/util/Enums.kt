41de2b55223d2bb9bac4fdbaf889b4c45a5651c9
canergures <gurescaner@gmail.com>
2022-08-11 21:55:16 +0300

    Created navigation compose component.

---
+++ app/src/main/java/com/example/todoappcompose/util/Enums.kt
@@ -0,0 +1,12 @@
+package com.example.todoappcompose.util
+
+class Enums {
+    enum class Action {
+        ADD,
+        UPDATE,
+        DELETE,
+        DELETE_ALL,
+        UNDO,
+        NO_ACTION
+    }
+}
